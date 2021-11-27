0162ccd08d1f65d4021937bcbc30e72e1f71c04f
Mykola Orliuk <virkony@gmail.com>
2021-11-27 15:11:25 +0100

    Flakify pidgin-chime plugin

--- flake.nix
+++ flake.nix
@@ -1,9 +1,19 @@
 {
   description = "Cherry-picked components from ony's Nix configs";
 
-  outputs = { self }: {
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: {
     homeManagerModules = {
       neovim-coc = import ./home/modules/neovim-coc.nix;
     };
-  };
+  }
+  //
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.pidgin-chime = pkgs.callPackage ./pkgs/pidgin-chime.nix { };
+    });
 }
