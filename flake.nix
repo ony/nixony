f97870dd981bfdcdfdde71265818a2b7d875fa2c
Mykola Orliuk <virkony@gmail.com>
2021-12-23 04:32:52 +0100

    Overlay for mergin packages in nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,17 +4,21 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: {
+    overlay = final: prev: {
+      pidgin-chime = final.pkgs.callPackage ./pkgs/pidgin-chime.nix { };
+    };
     homeManagerModules = {
       neovim-coc = import ./home/modules/neovim-coc.nix;
       neovim-tree-sitter = import ./home/modules/neovim-tree-sitter.nix;
     };
-  }
-  //
-  flake-utils.lib.eachDefaultSystem (system:
+  } // flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
     in
     {
-      packages.pidgin-chime = pkgs.callPackage ./pkgs/pidgin-chime.nix { };
+      packages.pidgin-chime = pkgs.pidgin-chime;
     });
 }
