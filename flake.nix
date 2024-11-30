b39df6a51dd1bc3cc7727468762ff1af420bbfb7
Mykola Orliuk <virkony@gmail.com>
2024-11-30 09:30:09 +0100

    vimPlugins: treesitter playground/spellsitter

--- flake.nix
+++ flake.nix
@@ -9,6 +9,29 @@
       pidgin-chime = final.pkgs.callPackage ./pkgs/pidgin-chime.nix { };
       nix-script-ruby = final.pkgs.writeScriptBin "nix-script-ruby"
         (builtins.readFile ./scripts/nix-script-ruby);
+
+      vimPlugins = prev.vimPlugins // {
+        nvim-treesitter-playground = final.pkgs.vimUtils.buildVimPlugin {
+          name = "nvim-treesitter-playground";
+          src = final.pkgs.fetchFromGitHub {
+            owner = "nvim-treesitter";
+            repo = "playground";
+            rev = "787a7a8d4444e58467d6b3d4b88a497e7d494643";
+            hash = "sha256-YMINv064VzuzZLuQNY6HN3oCZvYjNQi6IMliQPTijfg=";
+          };
+        };
+
+        nvim-spellsitter = final.pkgs.vimUtils.buildVimPlugin {
+          name = "nvim-spellsitter";
+          dontBuild = true;
+          src = final.pkgs.fetchFromGitHub {
+            owner = "lewis6991";
+            repo = "spellsitter.nvim";
+            rev = "3458915f9cccc7a4c95f272793790628fcd49bf7";
+            hash = "sha256-Tedv5x35MuLftWg0q8bYMM7Jhw7dMImeFP90IfdfJuY=";
+          };
+        };
+      };
     };
     homeManagerModules = {
       neovim-coc = import ./home/modules/neovim-coc.nix;
@@ -23,7 +46,12 @@
     in
     {
       packages = {
-        inherit (pkgs) pidgin-chime nix-script-ruby;
+        inherit (pkgs) unionfarm pidgin-chime nix-script-ruby;
+        vimPlugins = {
+          inherit (pkgs.vimPlugins)
+            nvim-treesitter-playground
+            nvim-spellsitter;
+        };
       };
     });
 }
