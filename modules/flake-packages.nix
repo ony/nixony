c07e99f05b5a061f1daebab22b6656ddeb101f3c
Mykola Orliuk <virkony@gmail.com>
2026-06-28 10:57:25 +0200

    flake-packages: prototype packagesOverlay option

--- modules/flake-packages.nix
+++ modules/flake-packages.nix
@@ -1,6 +1,12 @@
-{ self, inputs, ... }:
+{
+  self,
+  lib,
+  config,
+  ...
+}:
+with (import ../lib { inherit lib; });
 let
-  inherit (self) lib;
+  inherit (lib) mkOption types;
 
   pkgsDir = "${self}/pkgs";
 
@@ -10,34 +16,47 @@ let
       final0.lib.getAttrFromPath path final0.pkgs
     );
 
-  pkgDefs = lib.mkPkgDefs (final0: {
-    unionfarm = final0.callPackage "${pkgsDir}/unionfarm.nix" { };
-    pidgin-chime = final0.callPackage "${pkgsDir}/pidgin-chime.nix" { };
-    overlayfs-tools = final0.callPackage "${pkgsDir}/overlayfs-tools.nix" { };
-    nix-script-ruby = final0.writeScriptBin "nix-script-ruby" (
-      builtins.readFile "${self}/scripts/nix-script-ruby"
-    );
-    ion-cli = final0.callPackage "${pkgsDir}/ion-cli.nix" { };
+  pkgDefs = mkPkgDefs config.packagesOverlay;
+  overlay = pkgDefs.toOverlay;
 
-    vimPlugins = {
-      nvim-spellsitter = nixpkgsAdopted "nvim-spellsitter" [ "vimPlugins" "spellsitter-nvim" ] final0;
-    };
-  });
+  overlayNodeType = types.oneOf [
+    (types.functionTo (types.functionTo overlayNodeType))
+    (types.lazyAttrsOf overlayNodeType)
+    types.raw # TODO: derivation
+  ];
 in
 {
-  flake.overlays.default = pkgDefs.toOverlay;
-
-  perSystem =
-    { system, pkgs, ... }:
-    {
-      # https://flake.parts/overlays.html?highlight=overlay#consuming-an-overlay
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [ self.overlays.default ];
-        config = { };
-      };
-
-      packages = pkgDefs.toFlatPackages pkgs;
-      checks = pkgDefs.toFlatPackages pkgs;
+  options = {
+    packagesOverlay = mkOption {
+      type = types.functionTo (types.lazyAttrsOf overlayNodeType);
     };
+  };
+
+  config = {
+    packagesOverlay = final0: {
+      unionfarm = final0.callPackage "${pkgsDir}/unionfarm.nix" { };
+      pidgin-chime = final0.callPackage "${pkgsDir}/pidgin-chime.nix" { };
+      overlayfs-tools = final0.callPackage "${pkgsDir}/overlayfs-tools.nix" { };
+      nix-script-ruby = final0.writeScriptBin "nix-script-ruby" (
+        builtins.readFile "${self}/scripts/nix-script-ruby"
+      );
+      ion-cli = final0.callPackage "${pkgsDir}/ion-cli.nix" { };
+
+      vimPlugins = {
+        nvim-spellsitter = nixpkgsAdopted "nvim-spellsitter" [ "vimPlugins" "spellsitter-nvim" ] final0;
+      };
+    };
+
+    flake.overlays.default = overlay;
+
+    perSystem =
+      { pkgs, ... }:
+      let
+        pkgs' = pkgs.extend overlay;
+      in
+      {
+        packages = pkgDefs.toFlatPackages pkgs';
+        checks = pkgDefs.toFlatPackages pkgs';
+      };
+  };
 }
